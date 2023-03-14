5ffe49b6b8da5d0263a812e429c7f7542815cada
nelulisefafis <rafaelasoares8p@gmail.com>
2023-03-14 08:56:52 -0400

    ifelse

---
+++ ifelsetest/ifelsetest/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ifelsetest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\t\t---Verifica se o número é par ou ímpar---\n");
+            Console.Write("Informe um número: ");
+            int num = int.Parse(Console.ReadLine());
+
+            if (num % 2 == 0)
+            {
+                Console.WriteLine("\n Este número par!");
+            }
+            else
+
+            {
+                Console.WriteLine("este número é impar");
+            }
+        }
+    }
+}
+
+
+
+
